50faa7de654b32fc996db75875d6a8fd15723bf5
Powell Quiring <powellquiring@gmail.com>
2021-01-06 18:08:17 +0000

    cloudshell fixes

--- actions/virtualenv_init.sh
+++ actions/virtualenv_init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
-pip3 install virtualenv
+python3 -m venv venv
+source venv/bin/activate
+pip install virtualenv
 virtualenv virtualenv
 source virtualenv/bin/activate
 pip install -r requirements_main.txt
