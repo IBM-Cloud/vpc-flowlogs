fdd1e0eaaffd5b2246a4068ede9cae72c66dce0c
Powell Quiring <powellquiring@gmail.com>
2021-12-15 11:51:08 -0800

    docker image publishing

--- actions/cli.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# Wrapper around the cli function in cli/__init__.py
-import cli
-
-if __name__ == "__main__":
-    cli.cli()
-
--- actions/virtualenv_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-python3 -m venv venv
-source venv/bin/activate
-pip install virtualenv
-virtualenv virtualenv
-source virtualenv/bin/activate
-pip install -r requirements_main.txt
